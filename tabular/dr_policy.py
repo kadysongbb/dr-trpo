1b4ae5663b7bc0c34b233ee4fb7bf9152f101c71
kadysongbb <kadysongbb@gmail.com>
2020-02-26 01:04:42 -0800

    change to use global optimization approach to find beta

--- tabular/dr_policy.py
+++ tabular/dr_policy.py
@@ -10,6 +10,7 @@ DRPolicyWass: Use Wasserstein Constraint.
 """
 
 import numpy as np
+from scipy import optimize
 from sklearn.linear_model import LinearRegression
 
 class DRPolicyKL(object):
@@ -24,6 +25,7 @@ class DRPolicyKL(object):
         self.sta_num = sta_num
         self.act_num = act_num
         self.distributions = []
+        self.delta = 0.01
         for i in range(sta_num):
             self.distributions.append(np.ones(act_num)/act_num)
 
@@ -57,29 +59,35 @@ class DRPolicyKL(object):
                 if count[s][i] != 0:
                     all_advantages[s][i] = all_advantages[s][i]/count[s][i]
 
+        def gradient(beta):
+            gradient = self.delta
+            for s in range(self.sta_num):
+                gradient += disc_freqs[s]*np.log(np.sum(np.exp(all_advantages[s]/beta)*self.distributions[s]))
+                numerator = np.sum(np.exp(all_advantages[s]/beta)*all_advantages[s]*self.distributions[s])
+                denom = beta*np.sum(np.exp(all_advantages[s]/beta)*self.distributions[s])
+                gradient -= disc_freqs[s]*numerator/denom
+            return gradient
+
+        def objective(beta):
+            objective = beta*self.delta
+            for s in range(self.sta_num):
+                objective += beta*disc_freqs[s]*np.log(np.sum(np.exp(all_advantages[s]/beta)*self.distributions[s]))
+            return objective
+
+
+        minimizer_kwargs = {"method": "BFGS", "jac": gradient}
+        beta = optimize.basinhopping(objective, 0.5, minimizer_kwargs=minimizer_kwargs,
+              niter=20)
+        beta = beta.x[0]
+        print(beta)
+
         # compute the new policy
-        beta = self.find_opt_beta(0.1, all_advantages, disc_freqs, 0.01, 0.1, 1e-2, 1000)
+        # beta = self.find_opt_beta(0.1, all_advantages, disc_freqs, 0.01, 0.1, 1e-2, 1000)
         old_distributions = self.distributions
         for s in range(self.sta_num):
             denom = np.sum(np.exp(all_advantages[s]/beta)*old_distributions[s])
             self.distributions[s] = np.exp(all_advantages[s]/beta)*old_distributions[s]/denom
 
-    def find_opt_beta(self, init_beta, all_advantages, disc_freqs, delta, gamma, precision, max_iter):
-        """Find optimal beta using gradient descent."""
-        cur_beta = init_beta
-        next_beta = init_beta + precision + 1e-3
-        for i in range(max_iter):
-            if abs(next_beta - cur_beta) <= precision:
-                break
-            cur_beta = next_beta
-            gradient = delta
-            for s in range(self.sta_num):
-                gradient += disc_freqs[s]*np.log(np.sum(np.exp(all_advantages[s]/cur_beta)*self.distributions[s]))
-                numerator = np.sum(np.exp(all_advantages[s]/cur_beta)*all_advantages[s]*self.distributions[s])
-                denom = cur_beta*np.sum(np.exp(all_advantages[s]/cur_beta)*self.distributions[s])
-                gradient -= disc_freqs[s]*numerator/denom
-            next_beta = cur_beta - gamma*gradient
-        return next_beta
 
 class DRPolicyWass(object):
     def __init__(self, sta_num, act_num):
@@ -95,6 +103,7 @@ class DRPolicyWass(object):
         self.distributions = []
         for i in range(sta_num):
             self.distributions.append(np.ones(act_num)/act_num)
+        self.delta = 0.01
             
     def sample(self, obs):
         """Draw sample from policy."""
@@ -128,18 +137,48 @@ class DRPolicyWass(object):
                 if count[s][i] != 0:
                     all_advantages[s][i] = all_advantages[s][i]/count[s][i]
 
-        # compute Q
-        # opt_beta = self.find_opt_beta(0.5, all_advantages, disc_freqs, 0.01, 0.1, 1e-2, 1000)
-        # opt_beta = self.find_opt_beta2(all_advantages)
-        if 'Taxi' in env_name:
-            opt_beta = 2 + 0.8*(np.random.random() - 0.5)
-        if 'Chain' in env_name:
-            opt_beta = 0.5
-        if 'Cliff' in env_name:
-            opt_beta = 0.5
-        best_j = self.find_best_j(opt_beta, all_advantages)
 
-        # compute the new policy 
+        def find_best_j(beta):
+            """Find argmax_j {A(s,aj) - β*d(aj,ai)}."""
+            best_j = [[0] * self.act_num for i in range(self.sta_num)]
+            for s in range(self.sta_num):
+                for i in range(self.act_num):
+                    opt_j = 0
+                    opt_val = all_advantages[s][opt_j] - beta*self.calc_d(opt_j,i)
+                    for j in range(self.act_num):
+                        cur_val = all_advantages[s][j] - beta*self.calc_d(j,i)
+                        if cur_val > opt_val:
+                            opt_j = j
+                            opt_val = cur_val
+                    best_j[s][i] = opt_j
+            return best_j
+
+        def objective(beta):
+            objective = beta*self.delta
+            best_j  =  find_best_j(beta)
+            for s in range(self.sta_num):
+                for i in range(self.act_num):
+                    opt_j = best_j[s][i]
+                    objective += disc_freqs[s]*self.distributions[s][i]*(all_advantages[s][opt_j] - beta*self.calc_d(opt_j, i))
+            return  objective
+
+
+        # if 'Taxi' in env_name:
+        #     opt_beta = 2 + 0.8*(np.random.random() - 0.5)
+        # if 'Chain' in env_name:
+        #     opt_beta = 0.5
+        # if 'Cliff' in env_name:
+        #     opt_beta = 0.5
+        # best_j = self.find_best_j(opt_beta, all_advantages)
+
+        rranges = [(0,4)]
+        beta = optimize.dual_annealing(objective, rranges, maxiter = 20)
+        beta = beta.x[0]
+        print(beta)
+
+        # Q
+        best_j = find_best_j(beta)
+        # compute the new policy
         old_distributions = self.distributions
         self.distributions = []
         for i in range(self.sta_num):
@@ -156,42 +195,4 @@ class DRPolicyWass(object):
             return 0
         else:
             return 1
-
-    def find_best_j(self, beta, all_advantages):
-        """Find argmax_j {A(s,aj) - β*d(aj,ai)}."""
-        best_j = [[0] * self.act_num for i in range(self.sta_num)]
-        for s in range(self.sta_num):
-            for i in range(self.act_num):
-                opt_j = 0
-                opt_val = all_advantages[s][opt_j] - beta*self.calc_d(opt_j,i)
-                for j in range(self.act_num):
-                    cur_val = all_advantages[s][j] - beta*self.calc_d(j,i)
-                    if cur_val > opt_val:
-                        opt_j = j
-                        opt_val = cur_val
-                best_j[s][i] = opt_j
-        return best_j
-
-    def find_opt_beta(self, init_beta, all_advantages, disc_freqs, delta, gamma, precision, max_iter):
-        """Find optimal beta using gradient descent."""
-        cur_beta = init_beta
-        next_beta = init_beta + precision + 1e-3
-        for i in range(max_iter):
-            if abs(next_beta - cur_beta) <= precision:
-                break
-            cur_beta = next_beta
-            best_j = self.find_best_j(cur_beta, all_advantages)
-            gradient = delta 
-            for s in range(self.sta_num):
-                for i in range(self.act_num):
-                    gradient += -disc_freqs[s]*self.distributions[s][i]*self.calc_d(best_j[s][i], i)
-            next_beta = cur_beta - gamma*gradient
-        return next_beta
         
-    def find_opt_beta2(self, all_advantages):
-        """Assign optimal beta to be the mean of the advantage difference"""
-        total_advantage_diff = 0
-        for i in range(self.sta_num):
-            total_advantage_diff += np.max(all_advantages[i]) - np.min(all_advantages[i])
-        beta = total_advantage_diff/self.sta_num
-        return beta
